3339e3509f3468116c32ac00b98c099e27816fc5
Srinivasan <info.srinivasan@gmail.com>
2011-11-24 23:03:45 +0530

    base_code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DairyManagement::Application.config.secret_token = '0820c6d599bb83038e192e7c250cbe295ca874b8097e65c064a3413eb000437673b116f2f0f762134704ff611ced38722ad21260278521dabffff9bf30fd2bd0'
